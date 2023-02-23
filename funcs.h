13e9dd214e3163e7de39a525f1fcbf50ebec04eb
juliannea <112399053+juliannea@users.noreply.github.com>
2023-02-23 18:13:39 -0500

    Add files via upload

---
+++ funcs.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+std::string box_function(int , int );
+
+std::string checkerboard(int , int );
+
+std::string cross(int );
+
+std::string lower(int );
+
+std::string upper(int );
+
+std::string trapezoid(int , int );
+
+
+std::string checkerboard3(int , int );
